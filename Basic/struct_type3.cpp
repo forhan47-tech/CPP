e1596057ab1a0d454ff38dae558a3e0f378c20fe
forhan47-tech <forhan.csecu@gmail.com>
2025-09-06 02:14:47 +0600

    Initial commit with C++ project structure

--- Basic/struct_type3.cpp
+++ Basic/struct_type3.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 using namespace std;
 
-struct Employee {
+struct Student {
     string name;
-    int id;
-    float salary;
+    int roll;
+    float marks;
+
+    void display() const {
+        cout << "Name: " << name << ", Roll: " << roll << ", Marks: " << marks << endl;
+    }
 };
 
 int main() {
-    Employee emp[2] = { {"John", 101, 5000.5}, {"Emily", 102, 6200.0} };
-
-    for (int i = 0; i < 2; i++) {
-        cout << "Employee " << i + 1 << " - Name: " << emp[i].name
-             << ", ID: " << emp[i].id
-             << ", Salary: $" << emp[i].salary << endl;
-    }
-
+    Student s = {"Naimuddin", 101, 87.5};
+    s.display();
     return 0;
 }
