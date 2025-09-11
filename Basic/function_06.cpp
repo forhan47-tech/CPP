a524ae2342294d91427b5614cfe1f34ca9ed1a05
forhan47-tech <forhan.csecu@gmail.com>
2025-09-12 02:14:37 +0600

    Initial commit with C++ project structure

--- Basic/function_06.cpp
+++ Basic/function_06.cpp
@@ -7,6 +7,6 @@ inline int square(int x) {
 }
 
 int main() {
-    cout << "Square of 5: " << square(5) << endl;
+    cout << "Square of 5: " << square(5) << endl;// Compiler may replace this with: 5 * 5;
     return 0;
 }
